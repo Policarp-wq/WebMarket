e8d8993919dcfc0ef61d6f1e00da0f8f5dba3f79
Policarp <tatarkin-evgeniy@mail.ru>
2024-12-25 12:01:17 +0300

    Добавьте файлы проекта.

---
+++ Controllers/MyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMarket.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public abstract class MyController
+    {
+        protected 
+        [HttpGet("{id}")] 
+        public virtual Task<IActionResult> GetById(int id)
+        {
+
+        }
+    }
+}
